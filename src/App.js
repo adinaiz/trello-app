9d542bc403bd86df0a4c3a0074049bfc38a83484
ADINAI NIIAZBEKOVA <ADINAIZN@gmail.com>
2022-08-31 15:40:57 +0600

    add some details

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import LoginPage from "./components/LoginPage/LoginPage";
-import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import MainPage from "./components/MyMainPage/MainPage";
 import PageNotFound from "./components/NotFound/PageNotFound";
-import { Suspense } from "react";
-
 
 function App() {
    return (
       <BrowserRouter>
-         <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-               <Route path="/" element={<Navigate replace to="/login" />} />
-               <Route path="/login" element={<LoginPage />}></Route>
-               <Route path="/homepage" element={<MainPage />}></Route>
-               <Route path='*' element={<PageNotFound/>}/>
-            </Routes>
-         </Suspense>
+         <Routes>
+            <Route index element={<LoginPage />} />
+            <Route path="/homepage" element={<MainPage />} />
+            <Route path="*" element={<PageNotFound />} />
+         </Routes>
       </BrowserRouter>
    );
 }
